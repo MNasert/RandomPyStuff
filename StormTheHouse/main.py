3e44101df681ba019f15280dee6168d28fe7ea3a
Maximilian Nasert <maximian.nasert@gmx.de>
2020-05-06 14:46:08 +0200

    A small side project about automating a flash game
    A small autoclicker which clicks all enemies by searching for black pixels in a certain area of the screen

---
+++ StormTheHouse/main.py
@@ -0,0 +1,39 @@
+import StormTheHouse.tools as tools #Tools
+from pynput.mouse import Button, Controller  #Handling the mouse
+from pynput.keyboard import Key #Handling key events
+from pynput.keyboard import Controller as Con #Handling the keyboard
+from pynput.mouse import Listener #Listening to mouse actions
+import time #Everything related to time
+
+
+
+keyboard = Con() #Keyboard controller
+mouse = Controller() #Mouse controller
+
+def agent(): #Game bot
+    listener = Listener(on_click=tools.on_click) #Init of listener
+    listener.start() #Starting the listener in another thread in a non-blocking fashion
+    i=0 #Shoot and wait counter
+    aoe=[0,0] #Area of effect memory of last shot to not hit falling enemies
+    while(listener.running): #As long as the listener is running:
+        time.sleep(.03) #Wait 30 ms till next search
+        img = tools.getImg() #Get grayscale image to np array
+        pos = tools.findEnemy(img) #Search for a enemy and get position: [x,y]
+        if pos is not None and not tools.comparePos(pos,aoe): #If the bot didnt shoot at this position lately then and there are enemies:
+            i = i + 1 #Increment counter
+            aoe = pos #Memory is current enemies position
+            mouse.position = ((pos[0],pos[1])) #Put the pointer on pos on screen
+            mouse.click(Button.left,count=2) #Click the enemy twice
+        else: #If the bot did shoot at this position lately or there are no enemies then:
+            i = i + 1 #Increment counter
+            if i > 50: #If the counter is greater 50
+                mouse.position=((973,594)) #Position the mouse around the middle of the game window
+                keyboard.press(Key.space) #Press space
+                time.sleep(.06) #Wait 60 ms
+                keyboard.release(Key.space) #Release space
+                i=0 #Reset counter
+
+if __name__ == '__main__':
+    #Limits are different units, such as "machine gunners" these seem not to be converted to a scale that surpasses the a > 150(grayscale) clause
+    time.sleep(5) #Wait 5000 ms
+    agent() #Start the bot
---
+++ StormTheHouse/tools.py
@@ -0,0 +1,38 @@
+#Library for Getting stuff to work
+
+#Imports
+from pynput.mouse import Button #Handling button events
+from PIL import ImageGrab as ImageGrab #Library for getting screen images
+import numpy as np #Commonly used math library
+#Frame of the game
+#x=628 y=502
+#x2=1164 y2=643
+
+
+def getImg(): #Function to capture a picture and convert it to a np array
+    img = ImageGrab.grab(bbox=(628,502,1164,643)).convert("L") #Get a grayscale image
+    img = np.array(img) #Turn the image to a np array
+    img.reshape([643-502,1164-628]) #Reshape the picture to a 2D array
+    return img #Return the array
+
+def findEnemy(img): #Function to find enemies on screen
+    a = img.max() #Local variable for comparison, initialized as the greatest value of the input np array
+    if a > 150: #If the highest value is greater 150:
+        ind = np.unravel_index(img.argmax(),img.shape) #Find the index of the value
+        pos = [627+ind[1],500 +ind[0]-3] #Convert the index to a position on screen and turn values to x,y
+        return pos #Return the position on the screen in [x,y]
+    else: #If there is no value greater 150 on screen:
+        return None #Return None
+
+def on_click(x,y,button,pressed): #If the mouse is pressed while listener is running:
+    if button == Button.right: #If the pressed button is equals the right mouse button
+        print("Listener stopped") #Print something
+        return False #Return False- resulting in collapsing the listener thread
+
+def comparePos(pos,aoe): #Compare 2 arrays- here positions
+    q=0 #Local variable
+    if pos[0]<aoe[0]+30 and pos[0]>aoe[0]-30: #If the x distance of pos to aoe is between +-30:
+        q = q + 1 #Increment q
+    if pos[1]<aoe[1]+30 and pos[1]>aoe[1]-30: #If the y distance of pos to aoe is between +-30:
+        q = q + 1 #Increment q
+    return True if q>1 else False #If q is greater 1 return True otherwise return False
